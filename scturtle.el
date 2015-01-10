96853badf80a3527479fe3df54f99405f5006135
scturtle <scturtle@gmail.com>
2015-01-10 15:04:46 +0800

    Update scturtle.el

--- scturtle.el
+++ scturtle.el
@@ -1,17 +1,26 @@
-;; for Emacs Starter Kit
+;;; scturtle.el --- Emacs config of scturtle
+;; Copyright (C) 2015  Shen Chao
+;; Author: Shen Chao <scturtle@gmail.com>
+;; Keywords:
+;;; Commentary:
+;;; Code:
 
 ;; quick access to this config file
 (global-set-key (kbd "C-c e")
    '(lambda () (interactive)
-      (find-file (concat "~/.emacs.d/" (getenv "USER") ".el"))))
+      (find-file (concat user-emacs-directory (getenv "USER") ".el"))))
 
-;; toolbar
-(tool-bar-mode -1)
+;; bars
+(when (window-system)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1))
 
+;; global config
+(global-auto-revert-mode t)
+(setq size-indication-mode t)
 (setq column-number-mode t)
 (add-hook 'prog-mode-hook
   (lambda ()
-    (linum-mode)
     (setq show-trailing-whitespace t)))
 
 ;; mouse in terminal
@@ -23,21 +32,41 @@
 (prefer-coding-system 'chinese-gbk)
 (prefer-coding-system 'utf-8)
 
-;; font (osx)
-(set-default-font "Source Code Pro-18")
-(setq default-frame-alist '((font . "Source Code Pro-18")))
+;; font
+(set-frame-font "Source Code Pro-18" nil t)
 
 ;; theme
 (load-theme 'tango-plus t)
 
-;; persistent undo history
-(custom-set-variables '(undo-tree-auto-save-history t))
+;; linum mode
+(global-linum-mode 1)
+;; (setq linum-format "%3d")
+(set-face-background 'linum "#fcfcfc")
+(set-face-background 'fringe "#fcfcfc")
+
+;; highlight current line
+(global-hl-line-mode t)
+(set-face-background 'hl-line "#fcfcfc")
+
+;; transparency
+(set-frame-parameter (selected-frame) 'alpha '(99 99))
+(add-to-list 'default-frame-alist '(alpha 99 99))
+
+;; history
+(require 'undo-tree)
+(savehist-mode t) ;; minibuffer
+(setq undo-tree-auto-save-history t ;; undo
+      undo-tree-history-directory-alist
+        `(("." . ,(concat user-emacs-directory "undo"))))
 
 ;; turn off visual bell
 (setq ring-bell-function 'ignore)
 
 
 ;; --------- plugins -----------
+
+;; enable helm-mode
+(helm-mode 1)
 
 ;; color-theme-approximate
 (color-theme-approximate-on)
@@ -46,10 +75,8 @@
 (when (memq window-system '(mac ns))
       (exec-path-from-shell-initialize))
 
-; helm mode
-;; (require 'helm-config)
-
 ;; aspell
+(require 'ispell)
 (setq ispell-program-name "/usr/local/bin/aspell")
 
 ;; evil mode
@@ -57,7 +84,16 @@
 (global-evil-tabs-mode t) ;; evil-tabs package
 (evilnc-default-hotkeys)
 
-;; elscreen quick jump, index from 1
+;; evil-easymotion
+(require 'evil)
+(require 'ace-jump-mode) ;; FIEME: 'require' required?
+(evilem-default-keybindings "SPC")
+
+;; evil-tabs: cmd-t to open new tab and cmd-w to close
+(global-set-key (kbd "s-t") '(lambda () (interactive) (elscreen-create)))
+(global-set-key (kbd "s-w") '(lambda () (interactive) (elscreen-kill)))
+
+;; evil-tabs: elscreen quick jump, index from 1
 (global-set-key (kbd "s-1") '(lambda () (interactive) (elscreen-goto 0)))
 (global-set-key (kbd "s-2") '(lambda () (interactive) (elscreen-goto 1)))
 (global-set-key (kbd "s-3") '(lambda () (interactive) (elscreen-goto 2)))
@@ -68,7 +104,14 @@
 (global-set-key (kbd "s-8") '(lambda () (interactive) (elscreen-goto 7)))
 (global-set-key (kbd "s-9") '(lambda () (interactive) (elscreen-goto 8)))
 
+;; evil-tabs: tabbar color
+(custom-set-faces
+ '(elscreen-tab-background-face ((t (:background "#f0f0f0"))))
+ '(elscreen-tab-other-screen-face ((t (:background "#f0f0f0"
+                                       :foreground "black")))))
+
 ;; cursor color in evil mode
+(require 'evil)
 (setq evil-emacs-state-cursor '("red" box))
 (setq evil-normal-state-cursor '("black" box))
 (setq evil-visual-state-cursor '("orange" box))
@@ -83,17 +126,24 @@
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
 ;; company mode
+(require 'company)
 (add-hook 'prog-mode-hook 'company-mode)
 
 ;; flycheck
+(require 'flycheck)
 (add-hook 'prog-mode-hook 'flycheck-mode)
 
 ;; python
-(elpy-enable)
+(require 'elpy)
+;; FIXME: unkown issue of highlight-indentation
+(setq elpy-modules (remove 'elpy-module-highlight-indentation elpy-modules))
 (elpy-use-ipython)
-
+(elpy-enable)
+(add-to-list 'company-backends 'company-anaconda) ;; company-anaconda
 
 ;; ------- c++ start -------
+;; clang-format
+(global-set-key (kbd "C-c =") 'clang-format-region)
 
 ;; flycheck
 (setq flycheck-clang-language-standard "c++11")
@@ -120,15 +170,17 @@
 
 (global-set-key (kbd "C-x C-o") 'check-expansion)
 ;; (global-set-key (kbd "C-x C-o") 'company-complete)
+
+(require 'company-clang)
 (setq company-clang-arguments '("-std=c++11"))
 
 ;; company-c-headers
-(require 'company)
 (add-to-list 'company-backends 'company-c-headers)
 (require 'company-c-headers)
-(add-to-list 'company-c-headers-path-system "/usr/include/c++/4.2.1")
-;; (add-to-list 'company-c-headers-path-system "/Applications/Xcode.app/
-;; Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1")
+;; (add-to-list 'company-c-headers-path-system "/usr/include/c++/4.2.1")
+(add-to-list 'company-c-headers-path-system
+             (concat "/Applications/Xcode.app/Contents/Developer/Toolchains"
+                     "/XcodeDefault.xctoolchain/usr/include/c++/v1"))
 
 
 ;;  ------- haskell start -------
@@ -146,19 +198,20 @@
 
 ;; hotkey
 (eval-after-load 'haskell-mode '(progn
- (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
- (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
- (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
- (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
- (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
- (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
- (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
 
-(eval-after-load 'haskell-cabal '(progn
- (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
- (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
- (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
- (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
+(require 'haskell-cabal)
+  (eval-after-load 'haskell-cabal '(progn
+  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
 
 (eval-after-load 'haskell-mode
   '(define-key haskell-mode-map (kbd "C-c C-o") 'haskell-compile))
@@ -171,7 +224,6 @@
 (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
 ;; company mode for haskell
-(add-hook 'haskell-mode-hook 'company-mode)
 (add-to-list 'company-backends 'company-ghc)
 (custom-set-variables '(company-ghc-show-info t))
 
@@ -181,20 +233,27 @@
 ;; (set-face-background 'shm-current-face "#eee8d5")
 ;; (set-face-background 'shm-quarantine-face "lemonchiffon")
 
+;; flycheck
+;; FIXME: ghc checker doesn't work with sandbox
+(setq flycheck-checkers (delete 'haskell-ghc flycheck-checkers))
+(require 'flycheck-ghcmod)
+
 
 ;; ------- latex start -------
 
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
+(require 'tex)
+(require 'reftex)
+(setq TeX-auto-save t
+      TeX-parse-self t
+      reftex-plug-into-AUCTeX t
+      TeX-PDF-mode t
+      TeX-source-correlate-mode t
+      TeX-source-correlate-method 'synctex)
 (setq-default TeX-master nil)
 (add-hook 'LaTeX-mode-hook 'visual-line-mode)
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(setq reftex-plug-into-AUCTeX t)
-(setq TeX-PDF-mode t)
-(setq TeX-source-correlate-mode t)
-(setq TeX-source-correlate-method 'synctex)
 
 ;; skim (reverse search: cmd-shift-click)
 (setq TeX-view-program-list
@@ -213,9 +272,12 @@
 ;; (autoload 'coq-mode "coq" "Major mode for editing Coq vernacular." t)
 
 ;; proof-general
-(load-file "/usr/local/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
-(customize-set-variable 'proof-three-window-mode-policy 'hybrid)
+;; (load-file "/usr/local/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
+;; (customize-set-variable 'proof-three-window-mode-policy 'hybrid)
 
 ;; Racer - code completion for Rust
 ;; (setenv "DYLD_LIBRARY_PATH" "/usr/local/lib/rustlib/x86_64-apple-darwin/lib")
 ;; (add-to-list 'load-path "~/code/racer/editors") (require 'racer)
+
+(provide 'scturtle)
+;;; scturtle.el ends here
