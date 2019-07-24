9a89dad093f52d5e8fb14f15ceadc6a782d3bcf6
shenchao <shenchao@megvii.com>
2019-07-24 15:37:28 +0800

    split fish config into linux/mac version

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,10 +4,7 @@ set fish_greeting
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
-set PATH /Library/Developer/CommandLineTools/usr/bin $PATH
-set PATH ~/miniconda3/bin $PATH
 set PATH ~/.cargo/bin $PATH
-# set PATH ~/go/bin $PATH
 set PATH ~/.local/bin $PATH
 set PATH . $PATH
 
@@ -16,28 +13,9 @@ set PATH . $PATH
 set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
 #set -x CARGO_HOME ~/.cargo
 
-alias rm trash
-alias rrm /bin/rm
-alias vim /Applications/MacVim.app/Contents/MacOS/Vim
-alias emacs "/Applications/Emacs.app/Contents/MacOS/Emacs"
-alias emx "env TERM=xterm-24bits /Applications/Emacs.app/Contents/MacOS/Emacs -nw"
-alias pc "proxychains4 -q"
+alias emx "env TERM=xterm-24bits emacs -nw"
+alias pc proxychains4
 alias pp "env ALL_PROXY=socks5://localhost:1086"
+alias brew /home/linuxbrew/.linuxbrew/bin/brew
 
-# homebrew
-set -x HOMEBREW_NO_EMOJI 1
-set -x HOMEBREW_NO_AUTO_UPDATE 1
-set -x HOMEBREW_CASK_OPTS --appdir=/Applications
-set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
-#set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
-
-# powerline
-powerline-daemon -q
-set fish_function_path $fish_function_path ~/miniconda3/lib/python3.6/site-packages/powerline/bindings/fish
-powerline-setup
-
-# conda
-. ~/miniconda3/etc/fish/conf.d/conda.fish
-
-# iterm2
-. ~/.iterm2_shell_integration.fish
+source ~/miniconda3/etc/fish/conf.d/conda.fish
