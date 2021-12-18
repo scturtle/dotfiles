9dbddba871a40829d2132e9db4028eeafd4defbc
scturtle <scturtle@gmail.com>
2021-12-18 16:05:42 +0800

    kitty for linux

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,14 +11,9 @@ set PATH ~/.local/bin $PATH
 set PATH /home/linuxbrew/.linuxbrew/bin $PATH
 set PATH . $PATH
 
-# rust
-#set -x RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
-set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
-#set -x CARGO_HOME ~/.cargo
-
 alias ema "env LC_ALL=zh_CN.UTF-8 emacs"
-alias emx "env TERM=xterm-24bits emacs -nw"
-alias emc "env TERM=xterm-24bits emacsclient -t"
+alias emx "emacs -nw"
+alias emc "emacsclient -t"
 alias pc proxychains4
 alias pp "env ALL_PROXY=socks5://localhost:1086"
 alias ss "sudo systemctl"
