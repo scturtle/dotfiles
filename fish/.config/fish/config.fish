978ae8fbe0cc315506b3fc24be49f01bfa4006d1
shenchao <shenchao.0@bytedance.com>
2024-01-04 11:23:38 +0800

    nits

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,6 +12,5 @@ set PATH . $PATH
 
 alias emx "COLORTERM=truecolor emacs -nw"
 alias pc "proxychains4 -q"
-alias pp "env ALL_PROXY=socks5://127.0.0.1:1086"
 alias ss "sudo systemctl"
 alias sj "sudo journalctl"
